4632b65c0eb65588da4b54546a72747ad1121092
vlaksuga <45654321+vlaksuga@users.noreply.github.com>
2021-07-07 17:20:32 +0900

    poligon drawing

--- congresswww/js/main.js
+++ congresswww/js/main.js
@@ -33,6 +33,7 @@ window.onload = function() {
 
     const head = document.querySelector('section.head .container');
     const board = document.querySelectorAll('section.board .container ul li');
+    const canvas = document.getElementById('mycvs');
 
     if (head) { window.setTimeout(function(){ head.style.left = '0px'; }, 500); }
     if (board) {
@@ -40,5 +41,41 @@ window.onload = function() {
             window.setTimeout(() => { board[i].style.opacity = '1'; }, i*50 + 1000);
         }
     }
+
+    if(canvas) {
+        var ctx = canvas.getContext('2d');
+        const numberOfDepth = 5;
+        const numberOfSides = 5;
+        const bgColor = "grey";
+        const statsColor = "orange";
+        const Xcenter = 150;
+        const Ycenter = 150;
+        const sizeGrowth = 20;
+        var stats = [0,8,7,8,6,4]
+        
+        /* DRAW POLYGON */
+        for (var i = 1; i <= numberOfDepth; i++) {            
+            ctx.beginPath();
+            let size = i*sizeGrowth;
+            ctx.moveTo (Xcenter +  size * Math.sin(0), Ycenter +  size *  Math.cos(0));            
+            for (var ii = 1; ii <= numberOfSides; ii++) {
+                ctx.lineTo (Xcenter + size * Math.sin(ii * 2 * Math.PI / numberOfSides), Ycenter + size * Math.cos(ii * 2 * Math.PI / numberOfSides));                
+            }
+            ctx.strokeStyle = bgColor;
+            ctx.lineWidth = i*0.2;            
+            ctx.stroke();
+        }
+
+        /* DRAW STATS */
+            ctx.beginPath();
+            ctx.moveTo (Xcenter +  sizeGrowth*5 * Math.sin(0), Ycenter +  sizeGrowth*5 *  Math.cos(0));
+            for (var j = 1; j <= numberOfSides; j++) {
+                ctx.lineTo (Xcenter + stats[j]/2 * Math.sin(ii * 2 * Math.PI / numberOfSides), Ycenter + stats[j]/2 * Math.cos(ii * 2 * Math.PI / numberOfSides));                
+            }
+            ctx.strokeStyle = statsColor;
+            ctx.lineWidth = 2;            
+            ctx.stroke();
+    }
+    
 }
 
